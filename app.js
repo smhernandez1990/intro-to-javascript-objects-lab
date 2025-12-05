509be1751f4902e1686336d347c90756b1e2a65e
smhernandez1990 <samanthamaria.hernandez90@gmail.com>
2025-12-05 15:10:45 -0500

    exercise 15 complete

--- app.js
+++ app.js
@@ -275,3 +275,20 @@ game.partyCount = function(){
 }
 
 game.partyCount()
+
+/*
+Exercise 15
+1. Now, complete gyms with a difficulty below 8. Reflect on how this is similar to or different from the previous gym exercises.
+(change the value of `complete` in the qualifying objects from false to true).
+
+Solve Exercise 15 here:
+*/
+
+for (let i = 0; i < game.gyms.length; i++) {
+    if (game.gyms[i].difficulty < 8) {
+        game.gyms[i].completed = true
+        gymsCompleted.push(game.gyms[i])
+    }
+}
+console.log(gymsCompleted)
+console.log(game.gyms)
